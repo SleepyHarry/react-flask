73a17e52785233a04b874fa1b29389509ff3efff
SleepyHarry <harryavane@gmail.com>
2017-05-28 19:47:21 +0100

    update

--- js/Hello.js
+++ js/Hello.js
@@ -1,10 +1,9 @@
-import React from 'react';
+import React, {Component} from 'react';
 
-var Hello = React.createClass({
-  render() {
-    return <h1>Hello, world</h1>;
-  }
-});
+class Hello extends Component {
+    render() {
+        return <h1>Hello, world</h1>;
+    }
+}
 
 export default Hello;
-
